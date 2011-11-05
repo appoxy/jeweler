1fa5d908c35b70024479be3a5ca7a0e0bc45719a
Travis Reeder <treeder@gmail.com>
2011-11-05 00:11:17 -0700

    Using current branch instead of master

--- lib/jeweler/commands/release_gemspec.rb
+++ lib/jeweler/commands/release_gemspec.rb
@@ -25,7 +25,7 @@ class Jeweler
         commit_gemspec! if gemspec_changed?
 
         output.puts "Pushing current branch to origin"
-        repo.push
+        repo.push(repo.current_branch)
       end
 
       def clean_staging_area?
