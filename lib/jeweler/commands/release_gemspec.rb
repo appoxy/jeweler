a16ee4dce86b3e41b1822dc13cdd8850b65c8c37
Travis Reeder <travis@appoxy.com>
2011-11-06 21:01:16 -0800

    removed --others from git ls-files command when checking for clean staging area.

--- lib/jeweler/commands/release_gemspec.rb
+++ lib/jeweler/commands/release_gemspec.rb
@@ -31,8 +31,7 @@ class Jeweler
 
       def clean_staging_area?
         # surprisingly simpler than ruby-git
-        #`git ls-files --deleted --modified --others --exclude-standard` == ""
-        true
+        `git ls-files --deleted --modified --exclude-standard` == ""
       end
 
       def commit_gemspec!
